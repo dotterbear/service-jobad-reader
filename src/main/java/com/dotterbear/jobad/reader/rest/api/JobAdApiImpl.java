de6289eb8812adf09e5e45e2028e9eeedc73a20b
kan <kan@safghjkl@gmail.com>
2019-02-08 23:42:53 +0800

    add JobAdApiImpl

---
+++ src/main/java/com/dotterbear/jobad/reader/rest/api/JobAdApiImpl.java
@@ -0,0 +1,12 @@
+package com.dotterbear.jobad.reader.rest.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dotterbear.jobad.rest.api.JobadApi;
+
+@RestController
+@RequestMapping("/api/v1")
+public class JobAdApiImpl implements JobadApi {
+
+}
