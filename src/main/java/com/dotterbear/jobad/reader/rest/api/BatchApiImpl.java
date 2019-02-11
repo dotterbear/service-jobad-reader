ba752acdd572a895629e55da457aef68841d4d71
kan01234 <kan@safghjkl@gmail.com>
2019-02-11 19:38:54 +0800

    add batch job api endpoint

---
+++ src/main/java/com/dotterbear/jobad/reader/rest/api/BatchApiImpl.java
@@ -0,0 +1,12 @@
+package com.dotterbear.jobad.reader.rest.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dotterbear.jobad.rest.api.BatchApi;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BatchApiImpl implements BatchApi {
+
+}
