aaff288d2300c5012a2d125aa5255dc9d80ba967
kan01234 <safghjkl@gmail.com>
2019-02-12 10:45:14 +0800

    use application yml config instead of annatation

--- src/main/java/com/dotterbear/jobad/reader/JobAdRecorder.java
+++ src/main/java/com/dotterbear/jobad/reader/JobAdRecorder.java
@@ -13,7 +13,6 @@ import com.dotterbear.jobad.reader.data.model.JobAd;
 import com.dotterbear.jobad.reader.data.repo.JobAdRepository;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.dotterbear.jobad.feign.client")
 public class JobAdRecorder {
 
@@ -28,47 +27,6 @@ public class JobAdRecorder {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobAdRecorder.class, args);
-	}
-
-	
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-//		List<String> links = Arrays.asList(new String[] {
-//				"https://hk.jobsdb.com/hk/en/job/officer-assistant-officer-finance-100003006785941",
-//				"https://hk.jobsdb.com/hk/en/job/officer-assistant-officer-finance-100003006785941",
-//				"https://hk.jobsdb.com/hk/en/job/officer-assistant-officer-finance-100003006785941aaa",
-//				"https://hk.jobsdb.com/hk/en/job/graduate-trainee-100003006798490"
-//		});
-//		JobsDbHtmlReader jobsDbReader = new JobsDbHtmlReader();
-//		List<JobAd> jobAds = links.stream()
-//				.map(link -> jobsDbReader.convertDocumentToJobAd(link))
-//				.filter(Objects::nonNull)
-//				.collect(Collectors.toList());
-//		// TODO review batch insert
-//		mongoTemplate.bulkOps(BulkMode.UNORDERED, JobAd.class)
-//				.insert(jobAds)
-//				.execute();
-//		RssFeedClient rssFeedClient = Feign.builder()
-//				  .encoder(new GsonEncoder())
-//				  .decoder(new GsonDecoder())
-//				  .logger(new Slf4jLogger(RssFeedClient.class))
-//				  .target(RssFeedClient.class, "http://34.73.206.62:8762/");
-//		RssFeedDto rssFeedDto = rssFeedClient.fetchRSSFeed();
-//		if (rssFeedDto == null || rssFeedDto.getChannel() == null)
-//			return;
-//		List<String> links = rssFeedDto.getChannel().getItem()
-//				.stream().map(item -> item.getLink()).collect(Collectors.toList());
-//		JobsDbHtmlReader jobsDbReader = new JobsDbHtmlReader();
-//		List<JobAd> jobAds = links.stream()
-//				.map(link -> jobsDbReader.convertDocumentToJobAd(link))
-//				.filter(Objects::nonNull)
-//				.collect(Collectors.toList());
-//		// TODO review batch insert
-//		mongoTemplate.bulkOps(BulkMode.UNORDERED, JobAd.class)
-//				.insert(jobAds)
-//				.execute();
-//		System.out.println(rssFeedDto);
-		
 	}
 
 }
