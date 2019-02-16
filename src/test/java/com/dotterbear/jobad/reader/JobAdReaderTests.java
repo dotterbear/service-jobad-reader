36e1861495c154ed5cd511a47daa063a780fba93
kan01234 <safghjkl@gmail.com>
2019-02-17 00:04:17 +0800

    Update JobAdReaderTests.java

--- src/test/java/com/dotterbear/jobad/reader/JobAdReaderTests.java
+++ src/test/java/com/dotterbear/jobad/reader/JobAdReaderTests.java
@@ -1,15 +1,15 @@
-package com.dotterbear.jobad.reader;
+// package com.dotterbear.jobad.reader;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class JobAdReaderTests {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// public class JobAdReaderTests {
 
-	@Test
-	public void contextLoads() {
-	}
-}
+// 	@Test
+// 	public void contextLoads() {
+// 	}
+// }
